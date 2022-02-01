e8781ff0ab237b444aec0834f4787328894940e6
ilian <ilian@tuta.io>
2022-02-01 11:29:12 +0100

    Update to v2

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+        fmt.Fprintf(w, "Hello from version 2, you've requested: %s\n", r.URL.Path)
     })
 
     http.ListenAndServe(":8080", nil)
